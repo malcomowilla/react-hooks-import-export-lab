43b0a46414d46f79b6739c5b37f7a7998974fffb
malcomowilla <owillamalcom@gmail.com>
2023-05-08 19:10:35 +0300

    solved

--- src/data/user.js
+++ src/data/user.js
@@ -1,8 +1,8 @@
-const username = "Liza";
-const city = "New York";
+const username = "Malcom Owilla";
+const city = "Nairobi,Kenya";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
 
-export{username,city,image}
+export {username,city,image}
